eeef8586c957bf407b69eb57f4f02d2c9336ea89
Olav Liseth <olav@novelda.no>
2015-03-24 16:08:30 +0100

    Adds dsl script

---
+++ roadshowdsl.groovy
@@ -0,0 +1,40 @@
+gitusername = "badbitstuff"
+repo = "git@github.com:${username}/roadshow.git"
+prefix = "GEN"
+branch = "master"
+
+job("${gitusername}.${prefix}.build") {
+    scm {
+        git(repo, branch)
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        gradle('clean war jenkinstest jacoco')
+      shell("echo '\n\n#############\n\tBUILDING\n##############\n\n'")
+    }
+  	publishers {
+      	jacocoCodeCoverage()
+      	archiveJunit('build/test-results/*.xml')
+      	warnings(['Java Compiler (javac)'])
+    	downstream("${GITHUB_USER}.roadshow.generated.staticanalysis", 'SUCCESS')
+    }
+}
+
+job("${gitusername}.roadshow.${prefix}.staticanalysis") {
+    scm {
+        git(repo, branch)
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        gradle('clean staticanalysis')
+    }
+  	publishers {
+      checkstyle('build/reports/checkstyle/*.xml')	
+      pmd('build/reports/pmd/*.xml')
+      tasks('**/*', '', 'FIXME', 'TODO', 'LOW', true)
+  	}
+}
